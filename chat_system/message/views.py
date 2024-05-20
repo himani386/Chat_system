29cd46c0c010bc91526caebf64cb8ac7e0614da4
vishal <vishalkrsinghmsk@gmail.com>
2024-05-21 01:08:07 +0530

    added a template page and configured some urls

--- chat_system/message/views.py
+++ chat_system/message/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render ,HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("<h1>Hello World</h1>")
+
+
+def about(request):
+    return HttpResponse("<h1>About Page</h1>")
+
+
+def login(request):
+    return render(request , 'login.html' , {})
