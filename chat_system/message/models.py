43f4c2f87ba5da60a0137958e87a899c5c9da9e2
himani386 <97656486+himani386@users.noreply.github.com>
2024-05-21 16:49:02 +0530

    changes

--- chat_system/message/models.py
+++ chat_system/message/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    message=models.CharField(max_length=255)
+    sender=models.CharField(max_length=30)
+    reciver=models.CharField(max_length=30)
+
+
+
+
